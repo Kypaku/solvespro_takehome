e63f2febd477d60aeecbf0f85b8fb15dea288c3e
Kypaku <kypaku@mail.ru>
2020-11-09 12:44:27 +0300

    first changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - test_nuxt_ts',
-    title: 'test_nuxt_ts',
+    titleTemplate: '%s - SolvesPro',
+    title: 'SolvesPro - TakeHome',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -54,7 +54,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
